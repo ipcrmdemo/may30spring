90a21d8d49ac9ae86bb6d3d530291b6e0da877ab
Matt Cadorette <mcadorette@gmail.com>
2019-05-30 15:28:15 +0000

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/may30spring/May30springApplication.java
@@ -0,0 +1,12 @@
+package com.may30spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class May30springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(May30springApplication.class, args);
+	}
+}
---
+++ src/test/java/com/may30spring/May30springApplicationTests.java
@@ -0,0 +1,16 @@
+package com.may30spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class May30springApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
